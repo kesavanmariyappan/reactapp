ced7fbf5c2222eb827a8633e0038b681554f0b98
kesavanmariyappan <kesavan181003@gmail.com>
2023-12-08 11:21:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>this is kesavan</h1>
+      <p>
+        NAME:KESAVAN M</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        DEPARTMENT:INFORMATION TECHNOLOGY</p>
+        <p>YEAR:3</p>
+        <p>REGNO:73772121150</p>
+        <p>AREA OF INTEREST:BACK END PROGRAMMING
+      </p>
+      </div>
   );
 }
 
